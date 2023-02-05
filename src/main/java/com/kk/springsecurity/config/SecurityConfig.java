1a1c83ef5abdec5ce85c07aa5281dd86ee3ca136
kshitij khanal <k.kshitij.k.khanal@gmail.com>
2023-02-05 17:11:42 +0545

    documentation improved for securityconfig and passwordencoder separated

--- src/main/java/com/kk/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/kk/springsecurity/config/SecurityConfig.java
@@ -5,19 +5,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
-    /**
-     * custom config - copied from org.springframework.boot.autoconfigure.security.servlet.SpringBootWebSecurityConfiguration.class and modified
-     *
-     * @param http
-     * @return
-     * @throws Exception
-     */
+    // AbstractUserDetailsAuthenticationProvider - base class for authentication providers that implements common functionality for authentication.
+    // Step 1 - user enters credentials
+    // Step 2 - Spring Security Filters - default is org.springframework.boot.autoconfigure.security.servlet.SpringBootWebSecurityConfiguration
+    // Step 3 - AuthenticationManager - default is org.springframework.security.authentication.ProviderManager
+    // Step 4 - AuthenticationProvider - default is org.springframework.security.authentication.dao.DaoAuthenticationProvider
+    // Step 5.1 - UserDetailsService/UserDetailsManager - default is org.springframework.security.provisioning.InMemoryUserDetailsManager
+    // Step 5.2 - PasswordEncoder
+
+    // Step - 2
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((authorize) -> authorize
@@ -31,18 +35,26 @@ public class SecurityConfig {
         return http.build();
     }
 
+    // Step - 3 AuthenticationManager uses AuthenticationProvider
+    // Step - 4 AuthenticationProvider uses UserDetailsManager
+    // Step - 5.1 - UserDetailsManager
     @Bean
     public InMemoryUserDetailsManager userDetailsManager() {
-        UserDetails admin = User.withDefaultPasswordEncoder()
-                .username("admin")
+        UserDetails admin = User.withUsername("admin")
                 .password("123")
                 .authorities("admin")
                 .build();
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
+        UserDetails user = User.withUsername("user")
                 .password("123")
                 .authorities("read")
                 .build();
         return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    // Step - 4 AuthenticationProvider uses PasswordEncoder
+    // Step - 5.2 - PasswordEncoder
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
