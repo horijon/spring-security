ee3d26f9a09e650ee38f3c0487614eb8199dfa53
kshitij khanal <k.kshitij.k.khanal@gmail.com>
2023-02-05 15:58:16 +0545

    in memory multiple users

--- src/main/java/com/kk/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/kk/springsecurity/config/SecurityConfig.java
@@ -3,6 +3,9 @@ package com.kk.springsecurity.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -26,5 +29,20 @@ public class SecurityConfig {
         http.formLogin();
         http.httpBasic();
         return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("123")
+                .authorities("admin")
+                .build();
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("123")
+                .authorities("read")
+                .build();
+        return new InMemoryUserDetailsManager(admin, user);
     }
 }
