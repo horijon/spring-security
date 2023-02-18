3d857687e264cb31732c797a7d572cda48f3d0e3
kshitij khanal <k.kshitij.k.khanal@gmail.com>
2023-02-18 19:01:12 +0545

    JWT token based authentication and subsequent requests JWT token validation JWTTokenGeneratorFilter added JWTTokenValidatorFilter added JWT_HEADER i.e. Authorization exposed to cors origin i.e. 4200 in the response token generator only invoked for url /user token validator invoked for all urls except /user jjwt maven dependencies added spring security debugging and its logging removed as it is not suitable in production phase

--- src/main/java/com/kk/springsecurity/SpringSecurityApplication.java
+++ src/main/java/com/kk/springsecurity/SpringSecurityApplication.java
@@ -2,10 +2,9 @@ package com.kk.springsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
-@EnableWebSecurity(debug = true) // not recommended in prod
+//@EnableWebSecurity(debug = true) // not recommended in prod
 public class SpringSecurityApplication {
 
     public static void main(String[] args) {
